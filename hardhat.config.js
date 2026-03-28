e0d7d1df424b55652bae2fbb768bea30fdbb404f
Arife Çınar <arifecinar226@gmail.com>
2026-03-28 14:28:43 +0300

    feat: setup hardhat and deployment script for monad testnet

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz/",
+      chainId: 10143,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
